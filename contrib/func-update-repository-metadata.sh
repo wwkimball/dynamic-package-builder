af865aa9f47c5289bccefbc2314fb2fd5d13862b
William Kimball <wwbitbucket@kimballstuff.com>
2017-08-01 22:54:50 -0500

    Added a contrib function for updating repo metadata

---
+++ contrib/func-update-repository-metadata.sh
@@ -0,0 +1,29 @@
+################################################################################
+# Defines a function, updateRepositoryMetadata, which provides logic that can
+# generate or update RPM/yum repository metadata.
+################################################################################
+# Functions must not be directly executed
+if [ -z "${BASH_SOURCE[1]}" ]; then
+	echo "ERROR:  You may not call $0 directly." >&2
+	exit 1
+fi
+
+function updateRepositoryMetadata {
+	local repositoryDir=${1:?"ERROR:  The path to the top directory of an RPM repository must be provided as the first positional argument to ${FUNCNAME[0]}."}
+	mkdir -p "$repositoryDir"
+
+	# Update the repository metadata.  Try with SQLite, then without on failure.
+	if [ -d "$repositoryDir"/repodata ]; then
+		logInfo "Updating repository metadata for ${repositoryDir}."
+		if ! createrepo --database --update "$repositoryDir" 2>/dev/null; then
+			rm -rf "$repositoryDir"/.repodata
+			createrepo --no-database --update "$repositoryDir"
+		fi
+	else
+		logInfo "Creating new repository metadata for ${repositoryDir}."
+		if ! createrepo --database "$repositoryDir" 2>/dev/null; then
+			rm -rf "$repositoryDir"/{.repodata,repodata}
+			createrepo --no-database "$repositoryDir"
+		fi
+	fi
+}
