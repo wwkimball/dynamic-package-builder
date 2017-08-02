b9d567ffa7da023ed78152d01e7904653ef31454
William Kimball <wwbitbucket@kimballstuff.com>
2017-08-02 00:09:07 -0500

    Debugging...

--- lib/load-contrib-functions.sh
+++ lib/load-contrib-functions.sh
@@ -1,12 +1,6 @@
 ################################################################################
 # Extension library for ../build-rpm-specs.sh
 ################################################################################
-# Extension libraries must not be directly executed
-if [ -z "${BASH_SOURCE[1]}" ]; then
-	echo "ERROR:  You may not call $0 directly." >&2
-	exit 1
-fi
-
 # Import all functions from the rpm-helpers project, then the pwd, then the
 # SPECS contrib directories.
 for contribSource in "${_myDir}" . "${_globalSettings[SPECS_DIRECTORY]}"; do
