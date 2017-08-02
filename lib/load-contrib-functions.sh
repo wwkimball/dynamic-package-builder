6babe0b16f000113c3e809d57313d645295d5e69
William Kimball <wwbitbucket@kimballstuff.com>
2017-08-02 18:56:08 -0500

    Debugging...

--- lib/load-contrib-functions.sh
+++ lib/load-contrib-functions.sh
@@ -1,10 +1,17 @@
 ################################################################################
 # Extension library for ../build-rpm-specs.sh
 ################################################################################
+# Attempt to source the output logger functions
+if ! source "${_myLibDir}"/set-logger.sh; then
+	echo "ERROR:  Unable to import the logger source." >&2
+	exit 3
+fi
+
 # Import all functions from the rpm-helpers project, then the pwd, then the
 # SPECS contrib directories.
 for contribSource in "${_myDir}" . "${_globalSettings[SPECS_DIRECTORY]}"; do
 	contribDir="${contribSource}/contrib"
+	logDebug "Searching for contributed functions in directory:  ${contribDir}"
 	if [ -d "$contribDir" ]; then
 		while IFS= read -r -d '' funcFile; do
 			logDebug "Loading contributed function from source file:  ${funcFile}"
